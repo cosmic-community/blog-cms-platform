fd66ad41265dc88392f219a1370e7a03663906d7
Cosmic AI <support@cosmicjs.com>
2025-07-26 19:39:23 -0700

    Fix TypeScript errors: add proper parameter typing for dynamic routes and create missing CosmicBadge component

--- components/CosmicBadge.tsx
+++ components/CosmicBadge.tsx
@@ -65,8 +65,6 @@ export default function CosmicBadge({ bucketSlug }: CosmicBadgeProps) {
           src="https://cdn.cosmicjs.com/b67de7d0-c810-11ed-b01d-23d7b265c299-logo508x500.svg" 
           alt="Cosmic Logo" 
           className="w-5 h-5"
-          width={20}
-          height={20}
         />
         Built with Cosmic
       </a>
